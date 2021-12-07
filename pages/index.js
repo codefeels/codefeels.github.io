75dbaa362a94418cdd2c51f4273e90ba1c678ffa
Colin <colin.diesh@gmail.com>
2021-12-07 07:04:25 -0700

    Slugs

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,34 @@
 import { generateRSSFeed, getAllPosts } from '../lib/api'
 
 export default function AllPosts({ posts }) {
-  console.log({ posts })
+  console.log(JSON.stringify(posts))
   return (
-    <ul>
-      {posts.map(post => (
-        <li key={post.title}>{post.title}</li>
-      ))}
-    </ul>
+    <div>
+      <h2 style={{ fontFace: 'monospace' }}>codefeels</h2>
+      <ul>
+        {posts.map(post => (
+          <li key={post.data.title}>
+            <a href={post.data.slug}>{post.data.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
 export async function getStaticProps() {
-  const posts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
-  articles.sort((a, b) => (a.meta.date < b.meta.date ? 1 : -1))
-
-  generateRSSFeed(articles)
+  const posts = getAllPosts()
+  generateRSSFeed(posts)
 
   return {
-    props: { posts },
+    props: {
+      posts: posts.map(r => {
+        const { data, ...rest } = r
+        return {
+          ...rest,
+          data: { ...data, date: `${data.date.toLocaleString()}` },
+        }
+      }),
+    },
   }
 }
